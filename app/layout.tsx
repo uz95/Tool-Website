3bbb707d076de18439a0a5a082874f18bc9d8660
Usman <usmanzia90@gmail.com>
2026-05-28 12:00:09 -0700

    ADDED VERCEL ANALYTICS

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   metadataBase: new URL("https://www.fastwebtoolshub.com"),
